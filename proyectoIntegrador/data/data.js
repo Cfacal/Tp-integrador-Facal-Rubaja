ee40801d68f70e7d113212170d14f8cfe9e3c54f
Tinarubaja <mrubaja@udesa.edu.ar>
2023-04-05 11:45:11 -0300

    data

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -1,24 +1,126 @@
-const Email = document.getElementById('Email').value;
-const usuario = document.getElementById('usuario').value;
-const Contraseña = document.getElementById('Contraseña').value;
-const Fecha = document.getElementById('Fecha').value;
-const Documento = document.getElementById('Documento').value;
-const Foto = document.getElementById('Foto').value;
-
-
-const data = {
+let data ={
     usuario: {
-        Email: Email,
-        usuario: usuario,
-        Contraseña: Contraseña,
-        Fecha: Fecha,
-        Documento: Documento,
-        Foto: Foto
-    }
+        nombre: "Raul Perez",
+        email: "raulperez@gmail.com",
+        contrasena: "rperez234",
+        fecha_de_nacimiento:"04/23/1978",
+        documento:250986759,
+        //foto_de_perfil:
+    },
+    productos: [
+        "1" = {
+           // imagen:,
+            nombre_del_producto: "Top Infinite",
+            descripcion: "Top confeccionado 100% en tramado de morley en colores lisos. Fit al cuerpo, posee escote recto con hombros descubiertos y frunce en centro delantero ",
+            fecha_de_carga: "04/05/2023"
+        },
+        "2" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "3" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "4" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "5" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "6" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "7" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "8" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "9" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         },
+         "10" = {
+            // imagen:,
+            //  nombre_del_producto:,
+            //  descripcion: ,
+            //  fecha_de_carga:
+         }
+         
+    ],
+    comentarios:[
+        "1" = {
+            nombre_usario: "rperez",
+            texto_del_comentario: "Muy buena publicacion, me interesa",
+            //imagen_perfil: 
+        },
+        "2" = {
+            //nombre_usario: ,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "3" = {
+            //nombre_usario: ,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "4" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "5" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "6" = {
+            //nombre_usario: ,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "7" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "8" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "9" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        },
+        "10" = {
+            //nombre_usario:,
+            //texto_del_comentario:,
+            //imagen_perfil: 
+        }
+    ]
 }
-
-
-data.usuario
-
-export default data;
 
