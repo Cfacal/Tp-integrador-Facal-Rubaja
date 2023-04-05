3df1c5387b7dcd03132f31d3a5e7b491181ac27e
Cfacal <Cfacal@udesa.edu.ar>
2023-04-05 16:15:58 -0300

    arreglo modulo de datos

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -8,61 +8,71 @@ let data ={
         //foto_de_perfil:
     },
     productos: [
-        "1" = {
-           // imagen:,
+        {
+            id: "1",
+        //  imagen:,
             nombre_del_producto: "Top Infinite",
             descripcion: "Top confeccionado 100% en tramado de morley en colores lisos. Fit al cuerpo, posee escote recto con hombros descubiertos y frunce en centro delantero ",
             fecha_de_carga: "04/05/2023"
         },
-        "2" = {
+        {
+            id: "2",
             // imagen:,
             nombre_del_producto: "Pantalon Estrella",
             descripcion:" Jean 5 bolsillos tiro alto relajado, piernas rectas, con tajos laterales de ambos lados en altura de cadera. Bordes de bolsillos gastados." ,
             fecha_de_carga: "04/05/2023"
          },
-         "3" = {
+        {
+            id: "3",
             // imagen:,
             nombre_del_producto: "Buzo Rustic",
             descripcion:"Buzo cuello redondo de frisa liviana. Cuello y puños aplicados en ribb complemento. Detalle de abertura en puños para pasar los dedos" ,
             fecha_de_carga: "04/05/2023"
          },
-         "4" = {
+        {
+            id: "4",
             // imagen:,
             nombre_del_producto: "Botas Cowboy",
             descripcion: "Bota de PU simil gamuza. Plataforma de 4 cm. Taco de 11,5 cm. Con cierre posterior.",
             fecha_de_carga: "04/05/2023"
          },
-         "5" = {
-            // imagen:,
+         {
+            id: "5",
+            // imagen:
             nombre_del_producto: "Blazer Eco-cuero",
             descripcion: "Jacket en cuero ecologico forrado al tono. Cruce central con dos botones al tono. Bolsillos con tapa en delantero" ,
             fecha_de_carga: "04/05/2023"
          },
-         "6" = {
+         {
+            id: "6",
             // imagen:,
             nombre_del_producto: "Jean Iconic",
             descripcion:"Jean 5 bolsillos calce relax (tiro medio relajado y pierna recta larga), en color gris gastado con roturas sobre el ruedo." ,
             fecha_de_carga: "04/05/2023"
          },
-         "7" = {
+         {
+            id: "7",
             // imagen:,
             nombre_del_producto: "Top Pink",
             descripcion:"Top tipo corset de algodón con lycra. Recortes en delantero. Escote cuadrado con terminacion de collareta al tono de la base" ,
             fecha_de_carga:"04/05/2023"
          },
-         "8" = {
+         {
+            id: "8",
             // imagen:,
             nombre_del_producto: "Vestido Legally",
             descripcion: "Vestido corto de satén con sustracciones en cintura. Unión central en abdomen, en forma triangular",
             fecha_de_carga: "04/05/2023"
          },
-         "9" = {
+         { 
+            id: "9",
             // imagen:,
             nombre_del_producto: "Zapatillas Air",
             descripcion:"Zapatilla de PU con interior al tono, lengueta grande, cordones anchos. Base con camara de aire." ,
             fecha_de_carga: "04/05/2023"
          },
-         "10" = {
+         {
+            id: "10",
             // imagen:,
             nombre_del_producto: "Sandalias Flower",
             descripcion:"Sandalias de cuero sintético. Taco medio. Con pulsera fina regulable. Sin plataforma." ,
@@ -71,52 +81,52 @@ let data ={
          
     ],
     comentarios:[
-        "1" = {
+        {
             nombre_usario: "rperez",
             texto_del_comentario: "Muy buena publicación, me interesa",
             //imagen_perfil: 
         },
-        "2" = {
+        {
             nombre_usario: "laliesp" ,
             texto_del_comentario: "Me encanta, dame 1000",
             //imagen_perfil: 
         },
-        "3" = {
+        {
             nombre_usario: "tinipoliester" ,
             texto_del_comentario: "increible calidad, super comodo",
             //imagen_perfil: 
         },
-        "4" = {
+        {
             nombre_usario: "cfacal",
             texto_del_comentario: "espectacular",
             //imagen_perfil: 
         },
-        "5" = {
+        {
             nombre_usario: "falex",
             texto_del_comentario: "fenomenal, me gusta muchisimooo",
             //imagen_perfil: ƒ
         },
-        "6" = {
+        {
             nombre_usario:"Mora1243" ,
             texto_del_comentario: "estoy fascinada con mi compra",
             //imagen_perfil: 
         },
-        "7" = {
+       {
             nombre_usario: "sofigonet",
             texto_del_comentario: "super iconic, i like it very much",
             //imagen_perfil: 
         },
-        "8" = {
+        {
             nombre_usario: "juacovazquez",
             texto_del_comentario: "super chic",
             //imagen_perfil: 
         },
-        "9" = {
+        {
             nombre_usario: "nelsonkpo",
             texto_del_comentario: "muy buen producto chicos",
             //imagen_perfil: 
         },
-        "10" = {
+        {
             nombre_usario: "luchorockstar",
             texto_del_comentario: "colosalll",
             //imagen_perfil: 
