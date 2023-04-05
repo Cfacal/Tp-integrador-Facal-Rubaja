8678e37b96fb6e4ef58b92202d54df8cc9f44944
Cfacal <Cfacal@udesa.edu.ar>
2023-04-05 16:55:10 -0300

    arreglo css footer

--- proyectoIntegrador/routes/productos.js
+++ proyectoIntegrador/routes/productos.js
@@ -4,6 +4,6 @@ const ProductoControllers = require('../controllers/productoController');
 
 router.get('/detalle/:id', ProductoControllers.detalle)
 router.get('/agregar', ProductoControllers.agregar)
-router.get('/busqueda/:producto', ProductoControllers.busqueda)
+router.get('/busqueda', ProductoControllers.busqueda)
 
 module.exports = router;
