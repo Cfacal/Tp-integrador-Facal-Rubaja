c59481a341c4aec59405cac9577768f005928e78
Tinarubaja <mrubaja@udesa.edu.ar>
2023-04-05 19:10:38 -0300

    ruta imagenes vol2

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -5,7 +5,7 @@ let data ={
         contrasena: "rperez234",
         fecha_de_nacimiento:"04/23/1978",
         documento:250986759,
-        //foto_de_perfil:
+        foto_de_perfil:'/images/default-image.png'
     },
     productos: [
         {
@@ -24,56 +24,56 @@ let data ={
          },
         {
             id: "3",
-            //imagen:,
+            imagen:'/images/buzo.png',
             nombre_del_producto: "Buzo Rustic",
             descripcion:"Buzo cuello redondo de frisa liviana. Cuello y puños aplicados en ribb complemento. Detalle de abertura en puños para pasar los dedos" ,
             fecha_de_carga: "04/05/2023"
          },
         {
             id: "4",
-            // imagen:,
+            imagen:'/images/botas.png',
             nombre_del_producto: "Botas Cowboy",
             descripcion: "Bota de PU simil gamuza. Plataforma de 4 cm. Taco de 11,5 cm. Con cierre posterior.",
             fecha_de_carga: "04/05/2023"
          },
          {
             id: "5",
-            // imagen:
+            imagen:'/images/blazer.png',
             nombre_del_producto: "Blazer Eco-cuero",
             descripcion: "Jacket en cuero ecologico forrado al tono. Cruce central con dos botones al tono. Bolsillos con tapa en delantero" ,
             fecha_de_carga: "04/05/2023"
          },
          {
             id: "6",
-            // imagen:,
+            imagen:'/images/pantalonEstrella.png',
             nombre_del_producto: "Jean Iconic",
             descripcion:"Jean 5 bolsillos calce relax (tiro medio relajado y pierna recta larga), en color gris gastado con roturas sobre el ruedo." ,
             fecha_de_carga: "04/05/2023"
          },
          {
             id: "7",
-            // imagen:,
+            imagen: '/images/top.png',
             nombre_del_producto: "Top Pink",
             descripcion:"Top tipo corset de algodón con lycra. Recortes en delantero. Escote cuadrado con terminacion de collareta al tono de la base" ,
             fecha_de_carga:"04/05/2023"
          },
          {
             id: "8",
-            // imagen:,
+            imagen:'/images/vestido.png',
             nombre_del_producto: "Vestido Legally",
             descripcion: "Vestido corto de satén con sustracciones en cintura. Unión central en abdomen, en forma triangular",
             fecha_de_carga: "04/05/2023"
          },
          { 
             id: "9",
-            // imagen:,
+            imagen:'/images/zapatillas.png',
             nombre_del_producto: "Zapatillas Air",
             descripcion:"Zapatilla de PU con interior al tono, lengueta grande, cordones anchos. Base con camara de aire." ,
             fecha_de_carga: "04/05/2023"
          },
          {
             id: "10",
-            // imagen:,
+            imagen: '/images/sandalias.png',
             nombre_del_producto: "Sandalias Flower",
             descripcion:"Sandalias de cuero sintético. Taco medio. Con pulsera fina regulable. Sin plataforma." ,
             fecha_de_carga: "04/05/2023"
@@ -84,52 +84,52 @@ let data ={
         {
             nombre_usario: "rperez",
             texto_del_comentario: "Muy buena publicación, me interesa",
-            //imagen_perfil: 
+            imagen_perfil: '/images/default-image.png'
         },
         {
             nombre_usario: "laliesp" ,
             texto_del_comentario: "Me encanta, dame 1000",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "tinipoliester" ,
             texto_del_comentario: "increible calidad, super comodo",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "cfacal",
             texto_del_comentario: "espectacular",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "falex",
             texto_del_comentario: "fenomenal, me gusta muchisimooo",
-            //imagen_perfil: ƒ
+            imagen_perfil: '/images/default-image.png'
         },
         {
             nombre_usario:"Mora1243" ,
             texto_del_comentario: "estoy fascinada con mi compra",
-            //imagen_perfil: 
+            imagen_perfil: '/images/default-image.png' 
         },
        {
             nombre_usario: "sofigonet",
             texto_del_comentario: "super iconic, i like it very much",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "juacovazquez",
             texto_del_comentario: "super chic",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "nelsonkpo",
             texto_del_comentario: "muy buen producto chicos",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         },
         {
             nombre_usario: "luchorockstar",
             texto_del_comentario: "colosalll",
-            //imagen_perfil: 
+            imagen_perfil:'/images/default-image.png' 
         }
     ]
 }
