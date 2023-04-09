5bf52833a7fde284923db77d018d187a626b1f86
Cfacal <Cfacal@udesa.edu.ar>
2023-04-09 12:53:32 -0300

    Arreglo rutas dinámicas

--- proyectoIntegrador/routes/usuarios.js
+++ proyectoIntegrador/routes/usuarios.js
@@ -4,7 +4,7 @@ const usuariosControllers = require('../controllers/usuariosController');
 
 router.get('/ingresar', usuariosControllers.ingresar);
 router.get('/editar/:perfil', usuariosControllers.editar_perfil);
-router.get('/:perfil', usuariosControllers.ingresar_perfil);
+router.get('/perfil', usuariosControllers.ingresar_perfil);
 router.get('/registrarse', usuariosControllers.registrarse);
 
 module.exports = router;
