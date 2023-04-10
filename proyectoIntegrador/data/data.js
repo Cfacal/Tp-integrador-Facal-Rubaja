b1275445c989a1cb9b7523b518bb5b559f9001a5
Cfacal <Cfacal@udesa.edu.ar>
2023-04-10 15:46:16 -0300

    corrigo fechas

--- proyectoIntegrador/data/data.js
+++ proyectoIntegrador/data/data.js
@@ -3,7 +3,7 @@ let data ={
         nombre: "Raúl Perez",
         email: "raulperez@gmail.com",
         contrasena: "rperez234",
-        fecha_de_nacimiento:"04/23/1978",
+        fecha_de_nacimiento:"1978-04-23",
         documento:250986759,
         foto_de_perfil:'/images/default-image.png'
     },
@@ -13,70 +13,70 @@ let data ={
             imagen: '/images/remeraoversize.png',
             nombre_del_producto: "Remera Infinite",
             descripcion: "Remera de jersey, cuello redondo manga corta. Molderia oversize. Largo a la cadera. Estampa centrada en frente. Estampa en mangas.",
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
         },
         {
             id: 2,
             imagen: '/images/pantalonestrella.png',
             nombre_del_producto: "Pantalon Estrella",
             descripcion:" Jean 5 bolsillos tiro alto relajado, piernas rectas, con tajos laterales de ambos lados en altura de cadera. Bordes de bolsillos gastados." ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
         {
             id: 3,
             imagen:'/images/buzo.png',
             nombre_del_producto: "Buzo Rustic",
             descripcion:"Buzo cuello redondo de frisa liviana. Cuello y puños aplicados en ribb complemento. Detalle de abertura en puños para pasar los dedos" ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
         {
             id: 4,
             imagen:'/images/botas.png',
             nombre_del_producto: "Botas Cowboy",
             descripcion: "Bota de PU simil gamuza. Plataforma de 4 cm. Taco de 11,5 cm. Con cierre posterior.",
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          {
             id:5,
             imagen:'/images/blazer.png',
             nombre_del_producto: "Blazer Eco-cuero",
             descripcion: "Jacket en cuero ecologico forrado al tono. Cruce central con dos botones al tono. Bolsillos con tapa en delantero" ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          {
             id:  6,
             imagen:'/images/jeanGris.png',
             nombre_del_producto: "Jean Iconic",
             descripcion:"Jean 5 bolsillos calce relax (tiro medio relajado y pierna recta larga), en color gris gastado con roturas sobre el ruedo." ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          {
             id: 7,
             imagen: '/images/top.png',
             nombre_del_producto: "Top Pink",
             descripcion:"Top tipo corset de algodón con lycra. Recortes en delantero. Escote cuadrado con terminacion de collareta al tono de la base" ,
-            fecha_de_carga:"04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          {
             id: 8,
             imagen:'/images/vestido.png',
             nombre_del_producto: "Vestido Legally",
             descripcion: "Vestido corto de satén con sustracciones en cintura. Unión central en abdomen, en forma triangular",
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          { 
             id: 9,
             imagen:'/images/zapatillas.png',
             nombre_del_producto: "Zapatillas Air",
             descripcion:"Zapatilla de PU con interior al tono, lengueta grande, cordones anchos. Base con camara de aire." ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          },
          {
             id: 10,
             imagen: '/images/sandalias.png',
             nombre_del_producto: "Sandalias Flower",
             descripcion:"Sandalias de cuero sintético. Taco medio. Con pulsera fina regulable. Sin plataforma." ,
-            fecha_de_carga: "04/05/2023"
+            fecha_de_carga: "2023-04-05"
          }
          
     ],
