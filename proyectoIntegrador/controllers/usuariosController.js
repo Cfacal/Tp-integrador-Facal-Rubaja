514e8c5271644083a2e445636d5471ac34ffa0a1
Tinarubaja <mrubaja@udesa.edu.ar>
2023-05-25 10:14:44 -0300

    creacion de registracion de usuario

--- proyectoIntegrador/controllers/usuariosController.js
+++ proyectoIntegrador/controllers/usuariosController.js
@@ -1,4 +1,5 @@
 const usuarios = require('../data/data')
+const db = require('../database/models/index')
 const controlador = {
     ingresar: function(req,res){
         res.render('login', {UsuarioLogueado: false})
@@ -11,8 +12,27 @@ const controlador = {
     },
     registrarse: function(req,res){
         res.render('register', {UsuarioLogueado: false})
-    }
+    },
+    crear: function(req,res){
+        let {Email,usuario,Contraseña,Fecha,Documento,Foto} = req.body
+        db.Usuarios.create({
+            Email,
+            usuario,
+            Contraseña,
+            Fecha,
+            Documento,
+            Foto   
+        })
+        .then(function(data){
+            res.redirect('')
 
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+
+    }
 }
+
 
 module.exports = controlador;
--- proyectoIntegrador/routes/usuarios.js
+++ proyectoIntegrador/routes/usuarios.js
@@ -5,6 +5,7 @@ const usuariosControllers = require('../controllers/usuariosController');
 router.get('/ingresar', usuariosControllers.ingresar);
 router.get('/editar/:perfil?', usuariosControllers.editar_perfil);
 router.get('/perfil', usuariosControllers.ingresar_perfil);
+router.post('/registrase',usuariosControllers.crear);
 router.get('/registrarse', usuariosControllers.registrarse);
 
 module.exports = router;
