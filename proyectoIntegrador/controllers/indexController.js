1b56a6d0f91421202ebaafcf7c74dcd3567493ec
FelixAlexander02 <falexander@udesa.edu.ar>
2023-06-07 14:59:00 -0300

    home

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -1,5 +1,6 @@
 const productos = require("../data/data")
 const db = require('../database/models/index')
+const op = db.Sequelize.Op
 
 const controlador = {
     index: function(req,res){
@@ -10,13 +11,11 @@ const controlador = {
             raw: true
         })
         .then(function(data){
-            console.log(data)
+            res.render('inicio', {
+                     productos: data
+            })
         })
         .catch(function(err){console.log(err)})
-
-        res.render('inicio', {
-            productos: productos.productos, 
-            comentarios: productos.comentarios})
     },
     logout: function(req,res){
         req.session.usuario = undefined 
@@ -25,3 +24,11 @@ const controlador = {
 }
 
 module.exports = controlador;
+
+// res.render('inicio', {
+//     productos: productos.productos, 
+//     comentarios: productos.comentarios})
+
+// res.render('inicio', {
+//     productos: data
+// })
